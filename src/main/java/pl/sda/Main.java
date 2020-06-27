a5f39c3097d7e267fc9853fdda2913759542a507
Jakub <kubplon@gmail.com>
2020-06-28 01:53:40 +0200

    Initial commit

---
+++ src/main/java/pl/sda/Main.java
@@ -0,0 +1,14 @@
+package pl.sda;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO: generyki w klasach
+        //TODO: generyki w metodach
+        //TODO: generyki, a instancjonowanie nowego typu generycznego
+        //TODO: kiedy używać
+        //TODO: co jeśli nie podam generyku przy np. List
+        //TODO: z gwiazdką słówko extends i implements przy generykach
+        //TODO: zadanie z listą
+        //TODO: inny przykład z refleksją i zapisem dowolnego obiektu do pliku
+    }
+}
